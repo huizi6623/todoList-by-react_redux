8fab34936fbef83ce2fcf6d06913b5c75a4725f9
guhuihui <1254942166@qq.com>
2019-01-18 21:12:43 +0800

    webpack 打包

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React from 'react' ;
+import React, { Fragment } from 'react' ;
 import Footer from './Footer' ;
 import AddTodo from '../containers/AddTodo' ;
 import VisibleTodoList from '../containers/VisibleTodoList' ;
 
 const App = () => (
-    <div>
+    <Fragment>
         <AddTodo/>
         <VisibleTodoList/>
         <Footer/>
-    </div>
+    </Fragment>
 ) ;
 
 export default App ;
